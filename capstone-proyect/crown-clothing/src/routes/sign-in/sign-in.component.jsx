6281148789e4fe7c6d913450935828ab6691bbad
unknown <ag_antro@hotmail.com>
2024-07-28 19:02:36 -0300

    último commit

--- capstone-proyect/crown-clothing/src/routes/sign-in/sign-in.component.jsx
+++ capstone-proyect/crown-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
@@ -8,10 +9,17 @@ const SignIn = () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+  const logGoogleRedirectUser = async () => {
+    const { user } = await signInWithGoogleRedirect();
+    console.log({ user });
+  };
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sig in with google pop up</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sig in with google Redirect
+      </button>
     </div>
   );
 };
