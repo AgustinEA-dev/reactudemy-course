2f6df5704692fcce18c29d62fa404951955eb40e
unknown <ag_antro@hotmail.com>
2024-08-12 08:23:15 -0300

    último commit

--- capstone-proyect-new/crown-clothing/src/contexts/user.context.jsx
+++ capstone-proyect-new/crown-clothing/src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { onAuthStateChangedListener  } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -8,5 +9,8 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUSer] = useState(null);
   const value = { currentUser, setCurrentUSer };
+
+  useEffect(() => {}, []);
+
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
